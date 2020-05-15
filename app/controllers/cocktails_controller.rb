0d36f10d4055230006ebbcf81068e79360fef97e
PasqualeTroise <troisepasquale1990@gmail.com>
2020-05-16 00:46:19 +0100

    Started with the front-end

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,12 +15,12 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
-    redirect_to cocktails_path
+    redirect_to root_path
   end
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
