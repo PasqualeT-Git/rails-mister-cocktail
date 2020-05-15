749701302df1d5d8e1ba8397e6b098d0fd015a27
PasqualeTroise <troisepasquale1990@gmail.com>
2020-05-15 12:35:34 +0100

    Fixed cocktail controller to add a cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -12,4 +12,15 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
   
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
