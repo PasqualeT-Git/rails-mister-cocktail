4f8e1849f75e282b7840015ebdb9c2042984d8ff
PasqualeTroise <troisepasquale1990@gmail.com>
2020-05-22 21:00:38 +0100

    Added some cocktail in the seed with an attached image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+puts "Creating the ingredient list"
+print"."
+sleep(1)
+print"."
+sleep(1)
+print"."
+sleep(1)
 
 open_file = open(url).read
 ingredients = JSON.parse(open_file)
@@ -16,3 +25,16 @@ ingredients.each do |ingredient|
     Ingredient.create(name: ing["strIngredient1"] )
   end
 end
+
+puts "Adding some cocktail"
+print"."
+sleep(1)
+print"."
+sleep(1)
+print"."
+sleep(1)
+
+file = URI.open("https://res.cloudinary.com/paky-images/image/upload/v1589585287/nb2oxe486vwsezjjvojey2lkhsg8.jpg")
+cocktail = Cocktail.new(name: "Pinco-pallino")
+cocktail.photo.attach(io: file, filename: 'cocktail.jpg', content_type: 'image/jpg')
+cocktail.save!
